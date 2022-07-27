64d10323761f09256e43e9f2525ee9c0f0a71c62
徐鑫浩 <60800578+FutureXXH@users.noreply.github.com>
2022-07-27 15:37:01 +0800

    Delete Lesson1Homework directory

--- Lesson1Homework/ProjectFile/Source/Lesson_1/Lesson_1.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Lesson_1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lesson_1, "Lesson_1" );
- 
